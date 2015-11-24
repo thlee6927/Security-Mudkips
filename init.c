ad61ce416e382de86fc7a30c8ec8240e22b9a8c2
Catherine Choe <cchoe12@terpmail.umd.edu>
2015-11-23 23:26:09 -0500

    poop poop

---
+++ init.c
@@ -0,0 +1,11 @@
+#include "ports.h"
+#include "bank.h"
+#include "atm.h"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char**argv) {
+	printf("This is poop\n");
+	return 0;
+}
